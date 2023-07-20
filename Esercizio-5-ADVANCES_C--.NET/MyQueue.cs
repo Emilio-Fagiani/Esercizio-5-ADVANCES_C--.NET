8ab982709730ec342a580102e948343be4bbefc4
emifa <emifa@LAPTOP-1TM449QB>
2023-07-20 12:02:35 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-5-ADVANCES_C--.NET/MyQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_5_ADVANCES_C__.NET
+{
+    public class MyQueue<T>
+    {
+        private Queue<T> queue;
+
+        public MyQueue()
+        {
+            queue = new Queue<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            queue.Enqueue(item);
+        }
+
+        public T Dequeue()
+        {
+            if (queue.Count == 0)
+            {
+                throw new InvalidOperationException("La coda è vuota.");
+            }
+
+            return queue.Dequeue();
+        }
+
+        public T Peek()
+        {
+            if (queue.Count == 0)
+            {
+                throw new InvalidOperationException("La coda è vuota.");
+            }
+
+            return queue.Peek();
+        }
+
+    }
+}
